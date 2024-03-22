6615bf2ceb892b549c10ab4e2a19d748ce821f5c
Rosa Coldbeck-Shackley (iidrcol) <iidrcol@frgeneseq03.had.sa.gov.au>
2024-03-22 13:55:56 +1030

    Tool version log script and bammix file annotation updates

--- bcl2deciPHEr_runner.sh
+++ bcl2deciPHEr_runner.sh
@@ -11,7 +11,7 @@ source /phe/tools/miniconda3/etc/profile.d/conda.sh
 conda activate phesiqcal
 
 ### Running bcl2fastq
-BCL=$(sbatch --job-name bcl2fastq --mem 100G --ntasks 16 --time 960:00:00 --wrap "bcl2fastq --sample-sheet $input --runfolder-dir $dir --mask-short-adapter-reads 0 --use-bases-mask Y150,I8,N10,Y150 --no-lane-splitting --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --output-dir $dir/BaseCalls")
+BCL=$(sbatch -w frgeneseq03 --job-name bcl2fastq --mem 100G --ntasks 16 --time 960:00:00 -D /scratch/decipher/log --wrap "bcl2fastq --sample-sheet $input --runfolder-dir $dir --mask-short-adapter-reads 0 --use-bases-mask Y150,I8,N10,Y150 --no-lane-splitting --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --output-dir $dir/BaseCalls")
 
 ### Create folder
 folder=$(awk -F ',' 'FNR == 4 {print $2}' $input)
@@ -59,8 +59,8 @@ source /phe/tools/miniconda3/etc/profile.d/conda.sh
 conda activate phevir2
 
 ### Running deciPHEr_QC and deciPHErNEG on slurm
-deciPHEr_QC=$(sbatch --dependency=afterok:${JOBID_BCL} --job-name deciPHEr_QC --mem 100G --ntasks 16 --time 960:00:00 -D /scratch/decipher/$folder --wrap "snakemake -j 16 --use-conda --configfile /scratch/decipher/$folder/config.yaml --snakefile /phe/tools/decipher/Snakefile_deciPHEr_QC " )
-deciPHErNEG=$(sbatch --dependency=afterok:${JOBID_BCL} --job-name deciPHErNEG --mem 50G --ntasks 16 --time 960:00:00 -D /scratch/decipher/$folder --wrap "snakemake -j 16 --use-conda --configfile /scratch/decipher/$folder/config.yaml --snakefile /phe/tools/decipher/Snakefile_deciPHErNEG ")
+deciPHEr_QC=$(sbatch --dependency=afterok:${JOBID_BCL} -w frgeneseq03 --job-name deciPHEr_QC --mem 100G --ntasks 16 --time 960:00:00 -D /scratch/decipher/$folder --wrap "snakemake -j 16 --use-conda --configfile /scratch/decipher/$folder/config.yaml --snakefile /phe/tools/decipher/Snakefile_deciPHEr_QC " )
+deciPHErNEG=$(sbatch --dependency=afterok:${JOBID_BCL} -w frgeneseq03 --job-name deciPHErNEG --mem 50G --ntasks 16 --time 960:00:00 -D /scratch/decipher/$folder --wrap "snakemake -j 16 --use-conda --configfile /scratch/decipher/$folder/config.yaml --snakefile /phe/tools/decipher/Snakefile_deciPHErNEG ")
 
 ## Identify job_id of deciPHEr_QC on slurm
 array=(${deciPHEr_QC// / })
@@ -73,9 +73,9 @@ source /phe/tools/miniconda3/etc/profile.d/conda.sh
 conda activate phevir2
 
 ### Running deciPHEr_HIV/HCV/HBV1 on slurm
-deciPHEr_HIV1=$(sbatch --dependency=afterok:${JOBID_deciPHEr_QC} --job-name deciPHEr_HIV1 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap "snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HIV_scripts/Snakefile_deciPHEr_HIV1 " )
-deciPHEr_HCV1=$(sbatch --dependency=afterok:${JOBID_deciPHEr_QC} --job-name deciPHEr_HCV1 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap "snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HCV_scripts/Snakefile_deciPHEr_HCV1 " )
-deciPHEr_HBV1=$(sbatch --dependency=afterok:${JOBID_deciPHEr_QC} --job-name deciPHEr_HBV1 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap "snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HBV_scripts/Snakefile_deciPHEr_HBV1 " )
+deciPHEr_HIV1=$(sbatch --dependency=afterok:${JOBID_deciPHEr_QC} -w frgeneseq03 --job-name deciPHEr_HIV1 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap "snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HIV_scripts/Snakefile_deciPHEr_HIV1 " )
+deciPHEr_HCV1=$(sbatch --dependency=afterok:${JOBID_deciPHEr_QC} -w frgeneseq03 --job-name deciPHEr_HCV1 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap "snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HCV_scripts/Snakefile_deciPHEr_HCV1 " )
+deciPHEr_HBV1=$(sbatch --dependency=afterok:${JOBID_deciPHEr_QC} -w frgeneseq03 --job-name deciPHEr_HBV1 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap "snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HBV_scripts/Snakefile_deciPHEr_HBV1 " )
 
 ## Identify job_id of deciPHEr_HIV1, deciPHEr_HCV1 and deciPHEr_HBV1 on slurm
 array=(${deciPHEr_HIV1// / })
@@ -94,6 +94,10 @@ source /phe/tools/miniconda3/etc/profile.d/conda.sh
 conda activate phevir2
 
 ### Running deciPHEr_HIV/HCV/HBV2 on slurm
-deciPHEr_HIV2=$(sbatch --dependency=afterok:${JOBID_deciPHEr_HIV1} --job-name deciPHEr_HIV2 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap " snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HIV_scripts/Snakefile_deciPHEr_HIV2 ")
-deciPHEr_HCV2=$(sbatch --dependency=afterok:${JOBID_deciPHEr_HCV1} --job-name deciPHEr_HCV2 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap " snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HCV_scripts/Snakefile_deciPHEr_HCV2 ")
-deciPHEr_HBV2=$(sbatch --dependency=afterok:${JOBID_deciPHEr_HBV1} --job-name deciPHEr_HBV2 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap " snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HBV_scripts/Snakefile_deciPHEr_HBV2 ")
+deciPHEr_HIV2=$(sbatch --dependency=afterok:${JOBID_deciPHEr_HIV1} -w frgeneseq03 --job-name deciPHEr_HIV2 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap " snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HIV_scripts/Snakefile_deciPHEr_HIV2 ")
+deciPHEr_HCV2=$(sbatch --dependency=afterok:${JOBID_deciPHEr_HCV1} -w frgeneseq03 --job-name deciPHEr_HCV2 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap " snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HCV_scripts/Snakefile_deciPHEr_HCV2 ")
+deciPHEr_HBV2=$(sbatch --dependency=afterok:${JOBID_deciPHEr_HBV1} -w frgeneseq03 --job-name deciPHEr_HBV2 --mem 50G --ntasks 8 --time 960:00:00 -D /scratch/decipher/$folder --wrap " snakemake -j 8 --use-conda --configfile /scratch/decipher/$folder/QC_checkpoint_config.yaml --snakefile /phe/tools/decipher/HBV_scripts/Snakefile_deciPHEr_HBV2 ")
+
+### print all tool versions to a file
+
+source /phe/tools/decipher/decipher_tool_version_record.sh  > /scratch/decipher/$folder/decipher_tool_versions_$folder.csv
---
+++ decipher_tool_version_record.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+source /phe/tools/miniconda3/etc/profile.d/conda.sh
+
+current_DateTime=$(date +'%d/%m/%Y  %R')
+
+echo Date, $current_DateTime 
+  
+# echo BACTERIAL WGS RUN ID, $folder
+echo Path to raw data,$dir
+
+echo RUN ID,$folder
+
+echo ' '
+
+echo 'Pipeline: decipher vX.X.X'
+
+echo ' '
+
+echo TOOLS,TOOL_VERSION,DATABASES,DB_VERSION
+
+##### phevir2 tools #####
+
+conda activate phevir2
+
+# FASTP
+FASTP=$(conda list | grep "fastp" | awk '{print $1","$2}')
+echo $FASTP
+
+# KRAKEN
+KRAKEN=$(conda list | grep "kraken2" | awk '{print $1","$2}')
+KRAKEN_DB=$(basename /scratch/kraken/k2_pluspf_20220607/)
+echo $KRAKEN,Kraken_k2_db,$KRAKEN_DB
+
+# SEQTK
+SEQTK=$(conda list | grep "seqtk" | awk '{print $1","$2}')
+echo $SEQTK
+
+# SAMTOOLS
+SAMTOOLS=$(conda list | grep "samtools" | awk '{print $1","$2}')
+echo $SAMTOOLS
+
+# IVAR
+IVAR=$(conda list | grep "ivar" | awk '{print $1","$2}')
+echo $IVAR
+
+##### metaphe tools #####
+conda activate metaphe
+
+# BOWTIE2
+BOWTIE2=$(conda list | grep "bowtie2" | awk '{print $1","$2}')
+echo $BOWTIE2
+
+##### phesiqcal tools #####
+conda activate phesiqcal
+
+# SPADES
+SPADES=$(conda list | grep "spades" | awk '{print $1","$2}')
+echo $SPADES
+
+# MINIMAP2
+MINIMAP=$(conda list | grep "minimap2" | awk '{print $1","$2}')
+echo $MINIMAP
+
+# ABRICATE
+ABRICATE=$(conda list | grep "abricate" | awk '{print $1","$2}')
+HCVcore_dbupdate=$(stat -c %y /phe/tools/miniconda3/envs/phesiqcal/db/HCVcore/sequences | cut -d' ' -f 1)
+echo $ABRICATE,HCVcore,$HCVcore_dbupdate
+
+##### phevir tools #####
+conda activate phevir
+
+QUAST=$(conda list | grep "quast" | awk '{print $1","$2}')
+echo $QUAST
+
+##### covid-phylogeny tools #####
+conda activate covid-phylogeny
+BAMMIX=$(conda list | grep "bammix" | awk '{print $1","$2}')
+echo $BAMMIX
+
+##### shiver variants pipeline version dates #####
+
+SHIVER=$(stat /phe/tools/decipher/HIV_scripts/shiver/ | grep "Modify" | cut -f 2 -d " ")
+SHIVER_DB=$(stat /phe/tools/decipher/HIV_scripts/shiver/MyRefAlignment.fasta | grep "Modify" | cut -f 2 -d " ")
+echo shiver,$SHIVER,shiver_reference_database,$SHIVER_DB
+
+SHCVER=$(stat /phe/tools/decipher/HCV_scripts/shcver/ | grep "Modify" | cut -f 2 -d " ")
+SHCVER_DB=$(stat /phe/tools/decipher/HCV_scripts/shcver/HCVRefAlignment_plusGT8.fasta | grep "Modify" | cut -f 2 -d " ")
+echo shcver,$SHCVER,shcver_reference_database,$SHCVER_DB
+
+SHBVER=$(stat /phe/tools/decipher/HBV_scripts/shbver/ | grep "Modify" | cut -f 2 -d " ")
+SHBVER_DB=$(stat /phe/tools/decipher/HBV_scripts/shbver/HBVRefAlignment.fasta | grep "Modify" | cut -f 2 -d " ")
+echo shbver,$SHBVER,shbver_reference_database,$SHBVER_DB
+
